c962b93f97c72a715fd3061f19e2d063f608334e
peisje <darjakovalenko5@gmail.com>
2025-05-14 17:37:49 +0300

    Update library N2.2.py

--- Library N2/library N2.2.py
+++ Library N2/library N2.2.py
@@ -1,17 +1,5 @@
-from colorama import init, Fore, Back, Style
-# print(Fore.CYAN + 'some cyan text')
-# print(Back.MAGENTA + 'and with a magenta background')
-# print(Style.DIM + 'and in dim text')
-# print(Style.RESET_ALL)
-# print('back to normal now')
-
+from colorama import Fore, Back, Style, init
 import emoji
-# print(emoji.emojize('Python is :pile_of_poo:'))
-# print(emoji.emojize('Python is :orangutan:', language='alias'))
-# print(emoji.demojize('Python is 😏'))
-# print(emoji.emojize("Python is fun :hamburger:"))
-# print(emoji.emojize("Python is fun :smirking_face:", variant="emoji_type"))
-# print(emoji.is_emoji("🥞"))
 
 
 init()
@@ -19,9 +7,21 @@ init()
 print("happy - 1")
 print("sad - 2")
 print("scared - 3")
-print("schoked - 4")
-mood = input(Fore.CYAN + 'choose what is your mood today? ')
+print("shocked - 4")
+print("tired - 5")
 
 
-if mood == 1:
-    print(Back.MAGENTA + 'cool you are happy')
+mood = input(Fore.CYAN + 'Choose what is your mood today? ' + emoji.emojize(":pile_of_poo:", language="alias"))
+
+if mood == "1":
+    print(Back.YELLOW + 'I am happy for you! ' + emoji.emojize(":zany_face:", language="alias"))
+elif mood == "2":
+    print(Back.BLUE + 'i feel sorry for you ' + emoji.emojize(":cry:", language="alias"))
+elif mood == "3":
+    print(Back.GREEN + 'Don’t be scared everything is okay ' + emoji.emojize(":fearful:", language="alias"))
+elif mood == "4":
+    print(Back.WHITE + 'Wow shocked? ' + emoji.emojize(":astonished:", language="alias"))
+elif mood == "5":
+    print(Back.MAGENTA + 'take a rest ' + emoji.emojize(":sleeping_face:", language="alias"))
+else:
+    print(Fore.RED + 'Please enter 1-5 ' + emoji.emojize(":enraged_face:", language="alias"))
